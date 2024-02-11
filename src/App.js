eeaa26f0ede06eb4bfb880d2f40f539c5a5ead4e
Jane Harrison <33559193+euripidean@users.noreply.github.com>
2024-02-11 15:02:24 -0800

    Tweaks for gh-pages

--- src/App.js
+++ src/App.js
@@ -4,24 +4,22 @@ import NextBlock from "./Components/NextBlock/NextBlock";
 import ScoreBoard from "./Components/ScoreBoard/ScoreBoard";
 import Controls from "./Components/Controls/Controls";
 import MessagePopup from "./Components/MessagePopup/MessagePopup";
+import header from "./images/header.png";
+import gif from "./images/paw-patrol-gif.gif";
 
 function App() {
   return (
     <div className="App">
       <div className="App">
         <header className="App-header">
-          <img src="/header.png" alt="Paw Patrol Tetris" />
+          <img src={header} alt="Paw Patrol Tetris" />
         </header>
         <GridBoard />
         <NextBlock />
         <ScoreBoard />
         <Controls />
         <MessagePopup />
-        <img
-          className="patrol"
-          src="/paw-patrol-gif.gif"
-          alt="Paw Patrol Tetris"
-        />
+        <img className="patrol" src={gif} alt="Paw Patrol Tetris" />
       </div>
     </div>
   );
